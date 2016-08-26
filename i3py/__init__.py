a7f98dd704b6ce2acc9862bf90bc850829175e51
Caagr98 <Caagr98@users.noreply.github.com>
2016-08-27 00:38:32 +0200

    Fix some i3ipc bugs

--- i3py/__init__.py
+++ i3py/__init__.py
@@ -2,9 +2,18 @@ from Xlib.display import Display
 import i3ipc
 import os
 import sys
+import i3py.util
 
 stdout = sys.stdout
 sys.stdout = os.fdopen(os.dup(sys.stderr.fileno()), "w", 1)
+sys.stderr = sys.stdout
+
+i3ipc.WorkspaceEvent = lambda data, conn: data
+i3ipc.GenericEvent = lambda data: data
+i3ipc.WindowEvent = lambda data, conn: data
+i3ipc.BarconfigUpdateEvent = lambda data: data
+i3ipc.BindingEvent = lambda data: data
+i3ipc.Con = lambda data, parent, conn: data
 
 display = Display()
 i3 = i3ipc.Connection()
@@ -12,3 +21,9 @@ i3 = i3ipc.Connection()
 def reload():
 	python = sys.executable
 	os.execl(python, python, *sys.argv)
+
+@i3py.util.OtherThread
+def _start():
+	i3.subscriptions = 0xFF
+	i3.main()
+_start()
