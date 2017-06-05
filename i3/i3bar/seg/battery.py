d612945f08e506e140311afe13ce6014fc05b3cb
Caagr98 <caagr98@gmail.com>
2017-06-05 22:48:11 +0200

    Fix battery script

--- i3/i3bar/seg/battery.py
+++ i3/i3bar/seg/battery.py
@@ -1,5 +1,4 @@
 import i3bar
-import configparser
 import time
 
 __all__ = ["Battery"]
@@ -7,8 +6,32 @@ class Battery(i3bar.Segment):
 	file = "/sys/class/power_supply/BAT0/uevent"
 
 	def getOutput(self):
+		battery = BatteryStatus(self.file)
+		charge = battery.energy_now / battery.energy_full
+		text = "⚡ {:.0f}%".format(100 * charge)
+
+		if battery.consumption:
+			charging = battery.status == "Charging"
+			symbol = {"Charging": "↑", "Discharging": "↓"}.get(battery.status, "")
+
+			if charging:
+				remaining = battery.energy_full - battery.energy_now
+			else:
+				remaining = battery.energy_now
+			remainingTime = time.strftime("%H:%M", time.gmtime(remaining / battery.consumption * 60**2))
+			text += " {}{:.2f}W ({})".format(symbol, battery.consumption, remainingTime)
+
+		out = {}
+		out["full_text"] = text
+		if charge < 0.15: # Rounding
+			out["urgent"] = True
+		return out
+
+class BatteryStatus:
+	def __init__(self, path):
+		import configparser
 		cfg = configparser.ConfigParser()
-		with open(self.file, "r") as f:
+		with open(path, "r") as f:
 			cfg.read_string("[_]\n" + f.read())
 		battery = {}
 		for k in cfg["_"]:
@@ -18,24 +41,14 @@ class Battery(i3bar.Segment):
 			except ValueError:
 				pass
 			battery[k.lower()[13:]] = val
+		def get(*props):
+			for prop in props:
+				if prop in battery:
+					return battery[prop]
+			return None
 
-		percent = 100 * battery["energy_now"] / battery["energy_full"]
-		if battery.get("power_now", 0) > 0:
-			charging = battery["status"] == "Charging"
-			symbol = "↑" if charging else "↓"
+		self.status = get("status")
+		self.energy_now = get("energy_now", "charge_now") / 1000000
+		self.energy_full = get("energy_full", "charge_full") / 1000000
+		self.consumption = get("power_now") / 1000000
 
-			consumption = battery["power_now"] / 10**6
-
-			if charging:
-				remaining = battery["energy_full"] - battery["energy_now"]
-			else:
-				remaining = battery["energy_now"]
-			remainingTime = time.strftime("%H:%M", time.gmtime(remaining / battery["power_now"] * 60**2))
-
-			out = {}
-			out["full_text"] = "⚡ {:.0f}% {}{:.2f}W ({})".format(percent, symbol, consumption, remainingTime)
-			if percent < 10:
-				out["urgent"] = True
-			return out
-		else:
-			return "⚡ {:.0f}%".format(percent)
