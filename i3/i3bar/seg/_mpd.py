cf9adbaf6abaae62032fbe42b4fc41f61635acad
Caagr98 <caagr98@gmail.com>
2017-06-05 23:41:42 +0200

    Fix persistent MPD connections
    My MPD log file grew to half a gig before I noticed anything.

--- i3/i3bar/seg/_mpd.py
+++ i3/i3bar/seg/_mpd.py
@@ -7,6 +7,7 @@ import os.path
 import re
 from mpd import MPDClient
 from contextlib import contextmanager
+import threading
 
 __all__ = ["MPD"]
 
@@ -42,6 +43,7 @@ def open():
 class MPD(i3bar.Segment):
 	def __init__(self, dmenu_color=None):
 		self.color = dmenu_color
+		self._mpd = threading.local()
 
 	def start(self):
 		i3bar.ipc.register("mpd-play", self.play)
@@ -49,18 +51,23 @@ class MPD(i3bar.Segment):
 		i3bar.ipc.register("mpd-album", self.album)
 		self.updateloop()
 
+	@property
+	def mpd(self):
+		if not hasattr(self._mpd, "mpd"):
+			self._mpd.mpd = MPDClient()
+			self._mpd.mpd.connect(os.getenv("MPD_HOST", "localhost"), os.getenv("MPD_PORT", None))
+		return self._mpd.mpd
+
 	@i3bar.util.OtherThread
 	def updateloop(self):
-		with open() as mpd:
-			while True:
-				mpd.idle()
-				i3bar.log("asdf")
-				i3bar.update(self)
+		while True:
+			self.mpd.idle()
+			i3bar.update(self)
 
 	def getOutput(self):
-		with open() as mpd:
-			status = mpd.status()
-			title = gettitle(mpd.currentsong())
+		status = self.mpd.status()
+		currentsong = self.mpd.currentsong()
+		title = gettitle(currentsong)
 		progress = len(title) * float(status["elapsed"]) / float(status["duration"])
 
 		segs = [
@@ -95,44 +102,39 @@ class MPD(i3bar.Segment):
 				self.album()
 
 	def seek(self, n):
-		with open() as mpd:
-			title = gettitle(mpd.currentsong())
-			mpd.seekcur(n / len(title) * float(mpd.status()["duration"]))
+		title = gettitle(self.mpd.currentsong())
+		self.mpd.seekcur(n / len(title) * float(self.mpd.status()["duration"]))
 
 	def stop(self):
-		with open() as mpd:
-			mpd.stop()
+		self.mpd.stop()
 
 	def play(self):
-		with open() as mpd:
-			if mpd.status()["state"] != "play":
-				mpd.play()
-			else:
-				mpd.pause()
+		if self.mpd.status()["state"] != "play":
+			self.mpd.play()
+		else:
+			self.mpd.pause()
 
 	def album(self):
-		with open() as mpd:
-			def id(a): return a
-			def select(path):
-				files = [("file" in f, f.get("file", f.get("directory"))) for f in mpd.lsinfo(path)]
-				vals = {}
-				for isfile, fpath in files:
-					name = os.path.basename(fpath)
-					if isfile:
-						name = stripfname(name)
-					else:
-						name += '/'
-					vals[name] = (isfile, fpath)
-				keys = [""] + sorted(vals, key=lambda l: (vals[l][0], l))
-				vals[""] = (True, path)
-				out = dmenu.show(keys, background_selected=self.color)
-				if out not in vals:
-					return None
-				isfile, path = vals[out]
-				return path if isfile else select(path)
+		def select(path):
+			files = [("file" in f, f.get("file", f.get("directory"))) for f in self.mpd.lsinfo(path)]
+			vals = {}
+			for isfile, fpath in files:
+				name = os.path.basename(fpath)
+				if isfile:
+					name = stripfname(name)
+				else:
+					name += '/'
+				vals[name] = (isfile, fpath)
+			keys = [""] + sorted(vals, key=lambda l: (vals[l][0], l))
+			vals[""] = (True, path)
+			out = dmenu.show(keys, background_selected=self.color)
+			if out not in vals:
+				return None
+			isfile, path = vals[out]
+			return path if isfile else select(path)
 
-			track = select("")
-			if track is not None:
-				mpd.clear()
-				mpd.add(track)
-				mpd.play()
+		track = select("")
+		if track is not None:
+			self.mpd.clear()
+			self.mpd.add(track)
+			self.mpd.play()
