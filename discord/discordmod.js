fa622d4f525d9ed8ac2b237cf426c6c6cf56a8ec
Caagr98 <caagr98@gmail.com>
2018-10-14 15:58:24 +0200

    Change discord to only css

--- discord/discordmod.js
+++ discord/discordmod.js
@@ -4,29 +4,15 @@
 let fs = require("fs");
 
 function discordmod(win) {
-	const web = win.webContents;
-	const plugindir = __dirname + "/plugin/";
-
-	function execJS(str) {
-		web.executeJavaScript(str);
-	}
-	function execCSS(str) {
-		execJS(`
-		(()=>{
-			let link = document.createElement("style");
-			link.innerHTML=${JSON.stringify(str)};
-			document.head.appendChild(link);
-		})();
-		`);
-	}
-	if(!fs.existsSync(plugindir))
-		fs.mkdirSync(plugindir);
-	web.on("dom-ready", () => {
-		fs.readdirSync(plugindir).forEach(name => {
-			if(name.endsWith(".js"))  execJS(fs.readFileSync(plugindir + name, "utf8"));
-			if(name.endsWith(".css")) execCSS(fs.readFileSync(plugindir + name, "utf8"));
-		});
-	});
+	const cssFile = __dirname + "/discord.css";
+	const cssSource = fs.readFileSync(cssFile, "utf8");
+	win.webContents.executeJavaScript(`
+	(()=>{
+		let link = document.createElement("style");
+		link.innerHTML=${JSON.stringify(cssSource)};
+		document.head.appendChild(link);
+	})();
+	`);
 }
 
 module.exports = discordmod;
