788d83bb103139caee8f276da664635ab580da0e
Caagr98 <Caagr98@users.noreply.github.com>
2020-12-16 16:33:36 +0100

    Clean up patches

--- icebar/default.nix
+++ icebar/default.nix
@@ -4,6 +4,15 @@
 with pkgs;
 let ps = pythonPackages; in
 
+let mkPatch = file: lineno: text: from: to: writeTextFile {
+	name = "${file}.patch";
+	text = let
+		linestr = toString lineno;
+		line1 = builtins.replaceStrings ["«»"] [from] text;
+		line2 = builtins.replaceStrings ["«»"] [to] text;
+	in "--- a/${file}\n+++ b/${file}\n@@ -${linestr},1 +${linestr},1 @@\n-${line1}\n+${line2}\n";
+}; in
+
 ps.buildPythonApplication rec {
 	name = "icebar";
 	version = "1.0";
@@ -24,26 +33,15 @@ ps.buildPythonApplication rec {
 		ps.psutil
 	];
 
-	patches = [ (writeTextFile {
-		name = "icebar.patch";
-		text = ''
-		--- a/icebar/simplealsa.py
-		+++ b/icebar/simplealsa.py
-		@@ -17,1 +17,1 @@
-		-	asound = c.CDLL("libasound.so")
-		+	asound = c.CDLL("${alsaLib.out}/lib/libasound.so")
-
-		--- a/icebar/widgets/temperature.py
-		+++ b/icebar/widgets/temperature.py
-		@@ -11,1 +11,1 @@
-		-	out = subprocess.check_output(["sensors", "-u"]).decode()
-		+	out = subprocess.check_output(["${lm_sensors.out}/bin/sensors", "-u"]).decode()
-
-		--- a/icebar/simplei3.py
-		+++ b/icebar/simplei3.py
-		@@ -11,1 +11,1 @@
-		-		proc = await asyncio.create_subprocess_exec("i3", "--get-socketpath", stdout=asyncio.subprocess.PIPE)
-		+		proc = await asyncio.create_subprocess_exec("${i3.out}/bin/i3", "--get-socketpath", stdout=asyncio.subprocess.PIPE)
-		'';
-	}) ];
+	patches = [
+		(mkPatch "icebar/simplealsa.py" 17
+			''	asound = c.CDLL("«»")''
+			"libasound.so" "${alsaLib}/lib/libasound.so")
+		(mkPatch "icebar/widgets/temperature.py" 11
+			''	out = subprocess.check_output(["«»", "-u"]).decode()''
+			"sensors" "${lm_sensors}/bin/sensors")
+		(mkPatch "icebar/simplei3.py" 21
+			''		proc = await asyncio.create_subprocess_exec("«»", "--get-socketpath", stdout=asyncio.subprocess.PIPE)''
+			"i3" "${i3}/bin/i3")
+	];
 }
